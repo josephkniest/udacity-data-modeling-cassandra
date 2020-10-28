23cb5be6958d607fc7e50f9dcb0aec3a37014daa
Joe <joe@yis-macbook-pro.lan>
2020-10-27 18:53:59 -0700

    Installation commands to go into image compilation

--- dockerfile
+++ dockerfile
@@ -8,6 +8,16 @@ RUN apt-get update
 
 RUN apt-get install -y sudo
 
+RUN apt-get install -y python3.7
+
+RUN apt-get install -y python3-pip
+
+RUN apt-get install -y libpq-dev
+
+RUN apt-get install -y nano
+
+RUN pip3 install cassandra-driver
+
 # Drop udacity project files into image
 
 COPY udacity/ /root
--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-docker run --name sparkify-container -d cassandra
-docker exec -it sparkify-container apt-get update
-docker exec -it sparkify-container apt-get install -y python3.7
-docker exec -it sparkify-container apt-get install -y python3-pip
-docker exec -it sparkify-container apt-get install -y libpq-dev
-docker exec -it sparkify-container apt-get install -y nano
-docker exec -it sparkify-container pip3 install cassandra-driver
-
+docker run --name sparkify-container -d sparkify
+docker exec -it sparkify-container sudo -i

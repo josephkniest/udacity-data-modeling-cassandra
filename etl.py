c9d52b1b14534ce75c670988a10ece9214ac988c
Joe <joe@yis-macbook-pro.lan>
2020-10-28 15:22:40 -0700

    Create songplays table

--- etl.py
+++ etl.py
@@ -9,6 +9,24 @@ def main():
 
     print(files[2])
 
+    from cassandra.cluster import Cluster
+    cluster = Cluster()
+    session = cluster.connect()
+
+    session.execute('DROP KEYSPACE IF EXISTS sparkify')
+
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS sparkify
+        WITH REPLICATION = {
+           'class' : 'SimpleStrategy',
+           'replication_factor' : 1
+        };
+    """)
+
+    session.execute('USE sparkify')
+
+    session.execute('CREATE TABLE IF NOT EXISTS sparkify.songplays(id UUID PRIMARY KEY)')
+
 if __name__ == "__main__":
     main()
 
